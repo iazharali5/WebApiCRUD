25fc79ebb7314e8770cc413a584bda4f4a0e4cd7
Azhar Ali <Azhar Ali@DESKTOP-91HFN1K>
2022-02-09 12:53:00 +0530

    Add project files.

---
+++ Controllers/ApiCRUDController.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApiCRUD.Models;
+
+namespace WebApiCRUD.Controllers
+{
+    public class ApiCRUDController : ApiController
+    {
+        web_api_crud_dbEntities db =new web_api_crud_dbEntities();
+    
+        [HttpGet]
+        public IHttpActionResult GetEmpoyee()
+        {
+            List<Employee> list = db.Employees.ToList();
+            return Ok(list);
+        }
+
+        [HttpPost]
+        public IHttpActionResult InsertEmpoyee(Employee e)
+        {
+            db.Employees.Add(e);
+            db.SaveChanges();
+            return Ok();
+        }
+        [System.Web.Http.HttpGet]
+        public IHttpActionResult DetailsEmpoyeeById(int id)
+        {
+            var emp = db.Employees.Where(model =>model.id == id).FirstOrDefault();
+            return Ok(emp);
+        }
+
+        [HttpPut]
+        public IHttpActionResult UpdateEmpoyeeById(Employee e)
+        {
+            var emp = db.Employees.Where(model => model.id == e.id).FirstOrDefault();
+
+            if(emp!= null)
+            {
+                emp.id = e.id;
+                emp.name = e.name;
+                emp.age = e.age;
+                emp.designation = e.designation;
+                emp.salary = e.salary;
+                emp.gender = e.gender;
+                db.SaveChanges();
+
+            }
+            else
+            {
+                return NotFound();
+            }
+
+            return Ok();
+        }
+       [HttpDelete]
+        public IHttpActionResult DeleteEmpoyeeById(int id)
+        {
+            var emp = db.Employees.Where(model => model.id == id).FirstOrDefault();
+            db.Entry(emp).State = System.Data.Entity.EntityState.Deleted;
+            db.SaveChanges();
+            return Ok();
+        }
+
+
+
+
+
+
+
+    }
+}
---
+++ Controllers/CrudMvcController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Mvc;
+using WebApiCRUD.Models;
+
+namespace WebApiCRUD.Controllers
+{
+    public class CrudMvcController : Controller
+    {
+        // GET: CrudMvc
+        HttpClient client = new HttpClient();
+        public ActionResult Index()
+        {
+            List<Employee> emp_list = new List<Employee>();
+            client.BaseAddress = new Uri("https://localhost:44310/api/ApiCRUD");
+            var response = client.GetAsync("ApiCRUD");
+            response.Wait();
+            var test = response.Result;
+
+            if(test.IsSuccessStatusCode)
+            {
+                var display = test.Content.ReadAsAsync<List<Employee>>();
+                display.Wait();
+                emp_list = display.Result;
+            }
+          
+            return View(emp_list);
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        
+        [HttpPost]
+        public ActionResult Create(Employee emp)
+        {
+            client.BaseAddress = new Uri("https://localhost:44310/api/ApiCRUD");
+            var response = client.PostAsJsonAsync<Employee>("ApiCRUD",emp);
+            response.Wait();
+            var test = response.Result;
+
+            if (test.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View("Create");
+        }
+        public ActionResult Details(int id)
+        {
+            Employee e = null;
+            client.BaseAddress = new Uri("https://localhost:44310/api/ApiCRUD");
+            var response = client.GetAsync("ApiCRUD?id="+ id.ToString());
+            response.Wait();
+            var test = response.Result;
+
+            if (test.IsSuccessStatusCode)
+            {
+                var display = test.Content.ReadAsAsync<Employee>();
+                display.Wait();
+                e = display.Result;
+            }
+            return View(e);
+
+        }
+       public ActionResult Edit(int id)
+        {
+            Employee e = null;
+            client.BaseAddress = new Uri("https://localhost:44310/api/ApiCRUD");
+            var response = client.GetAsync("ApiCRUD?id=" + id.ToString());
+            response.Wait();
+            var test = response.Result;
+
+            if (test.IsSuccessStatusCode)
+            {
+                var display = test.Content.ReadAsAsync<Employee>();
+                display.Wait();
+                e = display.Result;
+            }
+            return View(e);
+        }
+        [HttpPost]
+        public ActionResult Edit(Employee e)
+        {
+            client.BaseAddress = new Uri("https://localhost:44310/api/ApiCRUD");
+            var response = client.PutAsJsonAsync<Employee>("ApiCRUD", e);
+            response.Wait();
+            var test = response.Result;
+
+            if (test.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View("Edit");
+
+        }
+
+        public ActionResult Delete(int id)
+        {
+            Employee e = null;
+            client.BaseAddress = new Uri("https://localhost:44310/api/ApiCRUD");
+            var response = client.GetAsync("ApiCRUD?id=" + id.ToString());
+            response.Wait();
+            var test = response.Result;
+
+            if (test.IsSuccessStatusCode)
+            {
+                var display = test.Content.ReadAsAsync<Employee>();
+                display.Wait();
+                e = display.Result;
+            }
+            return View(e);
+        }
+        [HttpPost,ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            client.BaseAddress = new Uri("https://localhost:44310/api/ApiCRUD");
+            var response = client.DeleteAsync("ApiCRUD/" +id.ToString());
+            response.Wait();
+            var test = response.Result;
+
+            if (test.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View("Delete");
+
+        }
+
+
+
+
+    }
+}
